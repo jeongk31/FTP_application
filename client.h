a4b57fc2f367246b13295f8ce028ff33d71af67b
Jeong Kyu Lee <139590388+jeongk31@users.noreply.github.com>
2024-03-26 23:30:25 +0400

    Update client.h

--- client.h
+++ client.h
@@ -15,17 +15,13 @@
 
 #define PORT 21
 #define BUFFER_SIZE 1024
-#define DATA_PORT 2021
 
 
 /* client_utils.c */
 void	send_command(int client_socket, const char *command);
 int		countWords(const char *str);
-void send_port_command(int controlSocket);
+int send_port_command(int controlSocket);
 void	receiveResponse(int client_socket);
 char*	getName(const char *str);
-
-/* client_dataConnect.c */
-//add data connection related functions
 
 #endif
